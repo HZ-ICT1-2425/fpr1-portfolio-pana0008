40b963979d23a3ebd43f4f6eea0d84cf4a91a18a
pana0008 <pana0008@hz.nl>
2025-02-12 15:49:44 +0100

    Testing again

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <title>HZ HBO-ICT</title>
 
     {{-- Compiled assets --}}
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
 {{-- Navigation bar --}}
